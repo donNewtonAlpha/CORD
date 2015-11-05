8db45066635b0b6194459f9d04ab9672b36a37a0
Don <dn8474@att.com>
2015-11-05 14:49:17 -0500

    convert to async

--- Interfaces.js
+++ Interfaces.js
@@ -1,5 +1,5 @@
-var wanLink="em4.703";
-var lanLink="p2p1";
+var wanLink="";
+var lanLink="";
 var child_process=require("child_process");
 var StringDecoder = require('string_decoder').StringDecoder;
 var decoder = new StringDecoder('utf8');
@@ -7,27 +7,39 @@ module.exports = function(RED) {
     var child_process=require("child_process");
     var StringDecoder = require('string_decoder').StringDecoder;
     var decoder = new StringDecoder('utf8');
+
     function createInterface(container,link,mac,eth){
         var cmd="pipework "+link+" -i eth"+eth+" "+container+" udhcpc  "+mac;
-        var outcome=child_process.execSync(cmd);       
-        var strOutcome=decoder.write(outcome);
-        console.log(cmd+":"+strOutcome);
+        child_process.exec(cmd,function(error,stdout,stderr){
+              if(error!=null){
+                 console.log(cmd+"  "+stderr);
+              }else{
+                 console.log(cmd+" "+stdout);
+              }
+        });       
     }
     function createInterfaceStatic(container,link,mac,eth){
         var cmd="pipework "+link+" -i eth"+eth+" "+container+" 192.168.1.254/24  "+mac;
-        var outcome=child_process.execSync(cmd);       
-        var strOutcome=decoder.write(outcome);
-        console.log(cmd+":"+strOutcome);
+        child_process.exec(cmd,function(error,stdout,stderr){
+              if(error!=null){
+                 console.log(cmd+"  "+stderr);
+              }else{
+                 console.log(cmd+" "+stdout);
+              }
+        });       
     }
     function InterfacesNode(config) {
+        this.context = {global:RED.settings.functionGlobalContext || {}};
         RED.nodes.createNode(this,config);
         var node = this;
         this.on('input', function(msg) {
+            var hostInterfaces=this.context.global.interfaces;
+
             var container=msg.payload.Service+"_"+msg.payload.S_Tag+"_"+msg.payload.C_Tag;
-            var i;
+            var hostInterfaces=this.context.global.interfaces;
             var interfaces=msg.payload.Interfaces;
             if(interfaces.indexOf("wan")>=0){
-               createInterface(container,wanLink,"",interfaces.indexOf("wan")); 
+               createInterface(container,hostInterfaces["wan"],"",interfaces.indexOf("wan")); 
                console.log("built wan interface");
             }
        
@@ -36,9 +48,9 @@ module.exports = function(RED) {
                 switch(interfaces[i]){
                    case "lan":
                       console.log("lan interface");
-                      var link=lanLink+"."+msg.payload.S_Tag+"."+msg.payload.C_Tag;
+                      var link=hostInterfaces["lan"]+"."+msg.payload.S_Tag+"."+msg.payload.C_Tag;
                       if(msg.payload.Service.toLowerCase()=="vsg")
-                         createInterface(container,link,"76:eb:27:f6:47:f1",i);
+                         createInterfaceStatic(container,link,"76:eb:27:f6:47:f1",i);
                       else
                          createInterface(container,link,"",i);
                       break;
